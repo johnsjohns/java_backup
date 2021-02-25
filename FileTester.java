6f98ac8eeff9304d2eb8d9722e81a8436bc80352
johnsjohns <o-ana@hotmail.com>
2021-02-25 06:52:36 -0300

    testando copias de arquivo

--- FileTester.java
+++ FileTester.java
@@ -1,15 +1,30 @@
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+
 class FileTester {
     public static void main(String[] args){
-        File file = new File("arquivo.txt");
+        File file = new File("temp");
+        System.out.println(file.isDirectory()? "verdadeiro":"falso");
+        System.out.println("absolute path: " + file.getAbsolutePath());
+        System.out.println("path: " + file.getPath());
         try{
-            System.out.println(file.createNewFile()? "Arquivo criado":"Arquivo ja exite");
-            
+            System.out.println("canonical path: " + file.getCanonicalPath());
+        } catch(Exception e){
+
+        }
+        System.out.println("Parent path: " + file.getParent());
+        String arquivos[] = file.list();
+        for (String arq : arquivos){
+            System.out.println(arq);
+        }
+
+        File source = new File("temp/1.txt");
+        try {
+            FileUtils.copyFile(source, new File("temp2/1.txt"));
         } catch (IOException e){
             
         }
-        System.out.println(file.getAbsolutePath());
     }
 }
