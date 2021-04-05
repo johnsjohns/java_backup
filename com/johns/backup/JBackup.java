6afb888b325037f5a15223cebcd498e014916823
johnsjohns <o-ana@hotmail.com>
2021-04-05 01:16:30 -0300

    refatorado codigo

--- com/johns/backup/JBackup.java
+++ com/johns/backup/JBackup.java
@@ -15,27 +15,27 @@ public class JBackup implements Runnable {
     private List<Item> itens;
     private File destino;
     private JProgressBar progress;
+    private String barra;
 
     public JBackup(List<Item> itens, File destino, JProgressBar progress) {
         this.itens = itens;
         this.destino = destino;
         this.progress = progress;
+        barra = File.separator;
     }
 
     public void run() {
         progress.setMaximum(getTotal());
 
         for (Item item : itens) { File arq = new File(item.getDiretorio());
-        copiar(arq, destino.getPath());
+        copiar(arq, destino.getPath() + barra + arq.getName());
         }
 
 
     }
 
-    private void copiar(File arq, String dest ) {
+    private void copiar(File arq, String caminho ) {
 
-        String caminho = dest;
-        String barra = File.separator;
         File arqs[] = arq.listFiles();
         for(File arq_temp : arqs){
             if (arq_temp.isDirectory()) {
@@ -73,10 +73,9 @@ public class JBackup implements Runnable {
         return total;
     }
 
-    private List<String> listar(File arq, String dest) {
+    private List<String> listar(File arq, String Caminho) {
         List<String> arquivos = new ArrayList<String>();
         String caminho = dest;
-        String barra = File.separator;
         File arqs[] = arq.listFiles();
         for(File arq_tem : arqs){
             if(arq_tem.isDirectory()){
